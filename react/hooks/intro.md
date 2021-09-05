05bcdab94e79dee505e6f7b48710940dc3ed35ff
kunal pal <kunalp@invezta.com>
2021-09-05 20:04:21 +0530

    rules of hooks

--- react/hooks/intro.md
+++ react/hooks/intro.md
@@ -4,12 +4,11 @@
 
 We already have a class that works well then why hooks ....?
 
-- React does not offer a way to attached reusable logic to component thus for this purpose we use pattern such as render props and HOC (higher order component) 
+- React does not offer a way to attached reusable logic to component thus for this purpose we use pattern such as render props and HOC (higher order component)
 
-- Class does not minify very well and thus they make hot reloading flaky and unreliable 
+- Class does not minify very well and thus they make hot reloading flaky and unreliable
 
 - For most of the newer developer the concept of classes such as bind and this is very hard to understand
-
 
 ## Types of Hooks
 
@@ -19,9 +18,9 @@ We already have a class that works well then why hooks ....?
 
 - useEffect
 
-- useContex 
+- useContex
 
-#### Additional 
+#### Additional
 
 - useReducer
 
@@ -31,8 +30,28 @@ We already have a class that works well then why hooks ....?
 
 - useCallback
 
-- useLayoutEffect   
+- useLayoutEffect
 
 - useDebugValue
 
 - useImperativeHandle
+
+## Rules of hooks
+
+- Don't call hooks form java script file call it in react functional component
+
+- Don't ever break the order in which hooks are called (if you want it conditional then use the second arugement for that )
+
+  example // 🔴 We're breaking the first rule by using a Hook in a condition
+  if (name !== '') {
+  useEffect(function persistForm() {
+  localStorage.setItem('formData', name);
+  });
+  }
+
+  useEffect(function persistForm() {
+  // 👍 We're not breaking the first rule anymore
+  if (name !== '') {
+  localStorage.setItem('formData', name);
+  }
+  })
--- react/hooks/useEffect.js
+++ react/hooks/useEffect.js
@@ -1,35 +1,53 @@
-import React ,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from "react";
 
 export const useEffectExample = () => {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    ///// Will render every time ///////////////
-    useEffect(() => {
-        console.log('every time')
-    })
+  ///// Will render every time ///////////////
+  useEffect(() => {
+    console.log("every time");
+  });
 
-    /////// Will act as componentDidMount and componentWillMount /////////
-    
-    useEffect(() => {
-        console.log('component did mount and unmount')
-    },[])
+  /////// Will act as componentDidMount and componentWillMount /////////
 
-    /////// Will act as componentWillUnMount ////////////
+  useEffect(() => {
+    console.log("component did mount and unmount");
+  }, []);
 
-    useEffect(() => {
-        return() => {console.log('component will unmount')}
-    })
+  /////// Will act as componentWillUnMount ////////////
 
-    //// Will execute only when the value of count change /////////
+  useEffect(() => {
+    return () => {
+      console.log("component will unmount");
+    };
+  });
 
-    useEffect(() => {
-        console.log('value of count change to :---- ',count)
-    },[count])
+  //// Will execute only when the value of count change /////////
 
-    return(
-        <div>
-            <p>The count is :- {count}</p>
-            <button onClick={() => setCount(count +1) }>Increment Count</button>
-        </div>
-    )
+  useEffect(() => {
+    console.log("value of count change to :---- ", count);
+  }, [count]);
+
+  return (
+    <div>
+      <p>The count is :- {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment Count</button>
+    </div>
+  );
+};
+
+/**
+
+Note:- In class component wheather the componet should tigerr re-render or not is done by
+
+componentDidUpdate
+
+for example 
+
+componentDidUpdate(prevProps, prevState) {
+  if (prevState.count !== this.state.count) {
+    document.title = `You clicked ${this.state.count} times`;
+  }
 }
+
+ */
